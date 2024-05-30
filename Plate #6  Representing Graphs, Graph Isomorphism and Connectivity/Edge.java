075590b68e3bfe508bb1fbdcbfd3581dd7b0ac36
Rajj0712 <rrgo@mcm.edu.ph>
2024-05-31 02:52:35 +0800

    Update Edge.java

--- Plate #6  Representing Graphs, Graph Isomorphism and Connectivity/Edge.java
+++ Plate #6  Representing Graphs, Graph Isomorphism and Connectivity/Edge.java
@@ -1,18 +1,1 @@
 
-public class Edge {
-
-    public static int size() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'size'");
-    }
-
-    public int u;
-    public int v;
-    public int count;
-
-    public Edge get(int i) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'get'");
-    }
-
-}
